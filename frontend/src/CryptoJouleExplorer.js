f6790aaf1656fcbaba6f3efb11f2a33693076e86
Astral Arkitekt <astralarkitekt@gmail.com>
2024-08-11 02:59:25 -0500

    Failure #2 - fairly uniform placement, but not sizing or spacing correctly

--- frontend/src/CryptoJouleExplorer.js
+++ frontend/src/CryptoJouleExplorer.js
@@ -1,9 +1,13 @@
 import { hashCycle } from "../../src/lib/hashCycle.js";
 import ByteModalities from "../../src/lib/ByteModalities.js";
+
 import * as THREE from "three";
 // get sky and water
 import { Sky } from "three/examples/jsm/objects/Sky.js";
 import { Water } from "three/examples/jsm/objects/Water.js";
+// I want a soft bloom effect
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
+
 // I want the player to be able to walk around
 //import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls.js";
 // I want orbit controls
@@ -15,6 +19,7 @@ export default class CryptoJouleExplorer {
     this.txnData = txnData;
     this.cryptoJoule = cryptoJoule;
     this.blockInfo = blockInfo;
+    this.terrain = new THREE.Group();
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
@@ -23,7 +28,7 @@ export default class CryptoJouleExplorer {
       0.1,
       10000
     );
-    this.renderer = new THREE.WebGLRenderer();
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     this.renderer.setClearColor(0xff9900, 1); // Set the background color
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -36,8 +41,8 @@ export default class CryptoJouleExplorer {
       container.appendChild(this.renderer.domElement);
     }
 
-    this.camera.position.z = 250;
-    this.camera.position.y = 500;
+    this.camera.position.z = 5;
+    this.camera.position.y = 10;
 
     // Add orbit controls
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -49,191 +54,169 @@ export default class CryptoJouleExplorer {
   }
 
   init() {
-    this.lighting = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+    // add an environment
+    this.scene.fog = new THREE.Fog(0x000000, 1, 1000);
+
+    // add lighting
+    this.lighting = new THREE.HemisphereLight(0xffffbb, 0x080820, 15);
     this.scene.add(this.lighting);
+    // add a directional light
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 
     const sky = new Sky();
-    sky.scale.setScalar(10000);
+    sky.scale.setScalar(450000);
+
+    const phi = THREE.MathUtils.degToRad(90);
+    const theta = THREE.MathUtils.degToRad(180);
+    const sunPos = new THREE.Vector3().setFromSphericalCoords(1, phi, theta);
+    sky.material.uniforms.sunPosition.value = sunPos;
     this.scene.add(sky);
 
-    const terrain = new THREE.Group();
-    this.scene.add(terrain);
-
+    const terrain = this.calculateTerrain();
     this.terraform(terrain);
 
-    // look at the cube
-    this.camera.lookAt(terrain.position);
-
+    const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+    const water = new Water(waterGeometry, {
+        textureWidth: 512,
+        textureHeight: 512,
+        waterNormals: new THREE.TextureLoader().load("/textures/waternormals.jpg", function (texture) {
+            texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        }),
+        alpha: 1.0,
+        sunDirection: this.lighting.position.clone().normalize(),
+        sunColor: 0xffffff,
+        waterColor: 0x001e0f,
+        distortionScale: 3.7,
+        fog: this.scene.fog !== undefined
+    });
+    water.rotation.x = -Math.PI / 2;
+    this.scene.add(water);
+    
     this.animate = this.animate.bind(this);
     this.animate();
   }
 
-  terraform(terrain) {
-    // Placeholder for your terraform logic
-    const parcelArea = this.txnData.size * this.txnData.size; // size of the parcel = size exponent 2 (square)
-    const dominantScale = 3;
-    const subdominantScale = 2;
-    const tertiaryScale = 1;
-
-    let { dominant, subdominant, tertiary } =
-      this.cryptoJoule.triQuanta.getRanking();
-    dominant = parseInt(dominant, 16);
-    subdominant = parseInt(subdominant, 16);
-    tertiary = parseInt(tertiary, 16);
-
-    const arrayOfForces = [];
-    for (let i = 0; i < dominant; i++) {
-      arrayOfForces.push(dominant);
-    }
-
-    for (let i = 0; i < subdominant; i++) {
-      arrayOfForces.push(subdominant);
-    }
-
-    for (let i = 0; i < tertiary; i++) {
-      arrayOfForces.push(tertiary);
-    }
-
-    // figure out how many bytes you need to fill the parcel
-    const bytesNeeded = Math.round((arrayOfForces.length / 32) * 2 - 1);
-
-    // use the Soul Signature to advance to this parcel's index
-    let soulSignatures = [this.cryptoJoule.triQuanta.soulSignature];
-    let soulSignature = this.cryptoJoule.triQuanta.soulSignature;
-    let prevSoulSignature = this.cryptoJoule.triQuanta.soulSignature;
-
-    for (let i = 0; i < bytesNeeded; i++) {
-      const cycledHash = hashCycle(soulSignature, prevSoulSignature);
-      soulSignatures.push(cycledHash);
-      prevSoulSignature = cycledHash;
-    }
-
-    // slice off the number of bytes needed after joining and exploding the soul signatures into soulBytes
-    const terrainBytes = soulSignatures
-      .join("")
-      .match(/.{2}/g)
-      .slice(0, arrayOfForces.length);
-
-    // use the terrainBytes to select a force from the array for each byte, removing the force entry from the array
-    const terrainForces = [];
-    const terrainCubes = this.getInstancedCube(terrainBytes.length);
-    terrainBytes.forEach((byte, index) => {
-      // convert byte to percentage
-      const bytePercentage = parseInt(byte, 16) / 255;
-      // select the array index based on the percentage
-      const forceIndex = Math.floor(bytePercentage * arrayOfForces.length);
-      // get the force
-      let force = arrayOfForces[forceIndex];
-
-      // remove the force from the array
-      arrayOfForces.splice(forceIndex, 1);
-      let cubeForceFactor = 1;
-      switch (force) {
-        case dominant:
-          force = (dominantScale * force * parseInt(byte, 16)) % 255;
-          cubeForceFactor = dominantScale;
-          break;
-        case subdominant:
-          force = (subdominantScale * force * parseInt(byte, 16)) % 255;
-          cubeForceFactor = subdominantScale;
-          break;
-        case tertiary:
-          force = (tertiaryScale * force * parseInt(byte, 16)) % 255;
-          cubeForceFactor = tertiaryScale;
-          break;
-      }
-      // add the force to the terrainForces array
-      terrainForces.push(force);
-
-      // get the byte modalities of force
-      const byteModalities = ByteModalities.getModalitiesForByte(
-        index,
-        force,
-        parseInt(byte, 16)
-      );
-
-      // scale the cube at the index
-      terrainCubes.setMatrixAt(
-        index,
-        new THREE.Matrix4().makeScale(force + cubeForceFactor, ((byteModalities[5].value / 255) * force) + cubeForceFactor, force + cubeForceFactor)
-      );
-      // color the cube at the index
-      terrainCubes.setColorAt(
-        index,
-        new THREE.Color(
-          `hsl(${Math.floor((byteModalities[8].value / 255) * 360)}, 50%, 50%)`
-        )
-      );
-
-      if (index > 0) {
-        // now to determine the positioning of the cube
-        const sides = [1, 2, 3, 4];
-        const prevCube = new THREE.Matrix4();
-        terrainCubes.getMatrixAt(index - 1, prevCube);
-
-        // get the position of the previous cube
-        const prevPosition = new THREE.Vector3();
-        const prevScale = new THREE.Vector3();
-        const prevRotation = new THREE.Quaternion();
-        prevCube.decompose(prevPosition, prevRotation, prevScale);
-
-        // choose a side on the previous cube to attach the current cube
-        // using the byte modalities Reflection
-        const side = sides[Math.floor((byteModalities[4].value / 255) * sides.length)];
-        
-        // get the width of the previous cube and divide by 2 to get the positional offset
-        const prevWidth = prevScale.x;
-        const offset = (prevWidth / 2);
-
-        // get the position of the current cube
-        const currentCube = new THREE.Matrix4();
-        let position = new THREE.Vector3();
-        let rotation = new THREE.Quaternion();
-        let scale = new THREE.Vector3();
-        terrainCubes.getMatrixAt(index, currentCube)
-        currentCube.decompose(position, rotation, scale);
-
-        // position the cube based on the proper side of the previous cube using x and z offsets
-        // 1 = top, 2 = right, 3 = bottom, 4 = left
-        switch (side) {
-          case 1:
-            position = new THREE.Vector3(prevPosition.x, prevPosition.y, prevPosition.z + prevWidth);
-            break;
-          case 2:
-            position = new THREE.Vector3(prevPosition.x + prevWidth, prevPosition.y, prevPosition.z);
-            break;
-          case 3:
-            position = new THREE.Vector3(prevPosition.x, prevPosition.y, prevPosition.z - prevWidth);
-            break;
-          case 4:
-            position = new THREE.Vector3(prevPosition.x - prevWidth, prevPosition.y, prevPosition.z);
-            break;
-        }
-
-        // set the position of the cube makeTranslation?
-        terrainCubes.setMatrixAt(index, new THREE.Matrix4().makeTranslation(position.x, position.y, position.z));
-        // terrainCubes.setMatrixAt(index, new THREE.Matrix4().makeScale(scale.x, scale.y, scale.z));
-        terrainCubes.instanceMatrix.needsUpdate = true;
-
-      } else {
-        // position the cube at the origin
-        terrainCubes.setMatrixAt(
-          index,
-          new THREE.Matrix4().makeTranslation(0, 0, 0)
-        );
-      }
-    });
-
-    // add the terrainCubes to the terrain group
-    terrain.add(terrainCubes);
-
-    // add the terrain to the scene
-    this.scene.add(terrain);
+  getSoulBytes() {
+    return this.cryptoJoule.triQuanta.soulSignature.match(/.{2}/g);
   }
 
-  getInstancedCube(instances) {
+  calculateTerrain() {
+    const totalCubes = 768; // 3 bytes in triQuanta of parcel * 256 (the max value of a byte)
+
+    // get and calculate the triQuanta forces total value
+    const { dominant, subdominant, tertiary } =
+      this.cryptoJoule.triQuanta.getRanking();
+    const totalForces =
+      parseInt(dominant, 16) +
+      parseInt(subdominant, 16) +
+      parseInt(tertiary, 16);
+
+    // normalize the forces against the total forces
+    const dominantProportion = parseInt(dominant, 16) / totalForces;
+    const subdominantProportion = parseInt(subdominant, 16) / totalForces;
+    const tertiaryProportion = parseInt(tertiary, 16) / totalForces;
+
+    // calculate the number of cubes for each force against the total cubes
+    const dominantCubes = Math.floor(totalCubes * dominantProportion);
+    const subdominantCubes = Math.floor(totalCubes * subdominantProportion);
+    const tertiaryCubes = Math.floor(totalCubes * tertiaryProportion);
+
+    return { dominantCubes, subdominantCubes, tertiaryCubes };
+  }
+
+  terraform(terrain) {
+    // get the soul signature and soul bytes
+    const soulSignature = this.cryptoJoule.triQuanta.soulSignature;
+    const soulBytes = this.getSoulBytes();
+  
+    const totalCubes = terrain.dominantCubes + terrain.subdominantCubes + terrain.tertiaryCubes;
+  
+    const { dominant, subdominant, tertiary } = this.cryptoJoule.triQuanta.getRanking();
+  
+    // fill an array with the total number of cubes where the value is the appropriate force
+    const terrainArray = [];
+    for (let i = 0; i < totalCubes; i++) {
+      if (i < terrain.dominantCubes) {
+        terrainArray.push(dominant);
+      } else if (i < terrain.dominantCubes + terrain.subdominantCubes) {
+        terrainArray.push(subdominant);
+      } else {
+        terrainArray.push(tertiary);
+      }
+    }
+  
+    // extend the soul signature so there is at minimum 1 byte per cube
+    let extendedSoulSignature = soulSignature;
+    let previousSoulSignature = soulSignature;
+    while (extendedSoulSignature.length < totalCubes * 2) {
+      const cycledSoulSignature = hashCycle(soulSignature, previousSoulSignature);
+      extendedSoulSignature += cycledSoulSignature;
+      previousSoulSignature = cycledSoulSignature;
+    }
+    const extendedSoulBytes = extendedSoulSignature.match(/.{2}/g).slice(0, totalCubes);
+  
+    // create the cubes
+    const instances = totalCubes;
+    const cubes = this.getInstancedCubes(instances);
+  
+    // do layout to it
+    const terrainSizes = [];
+    let xOffset = 0;
+    let zOffset = 0;
+    for (let i = 0; i < extendedSoulBytes.length; i++) {
+      const index = i;
+      const soulInt = parseInt(extendedSoulBytes[i], 16);
+      const soulPercent = soulInt / 255;
+      const tArrIndex = Math.floor(soulPercent * terrainArray.length);
+      const terrainSize = parseInt(terrainArray[tArrIndex], 16);
+      terrainArray.splice(tArrIndex, 1);
+      terrainSizes.push(terrainSize);
+  
+      const byteModalities = ByteModalities.getModalitiesForByte(index, terrainSize, soulInt);
+      
+      // Ensure byteModalities values are valid numbers
+      const width = (byteModalities[0].value / 255) + 1;
+      const depth = (byteModalities[0].value / 255) + 1;
+      const height = (byteModalities[5].value / 255) + 1;
+      const color = (byteModalities[8].value / 255) * 360;
+      
+      if (isNaN(width) || isNaN(depth) || isNaN(height)) {
+        console.error(`Invalid values at index ${i}: width: ${width}, depth: ${depth}, height: ${height}`);
+        continue; // Skip this iteration if values are invalid
+      }
+  
+    //   console.log(`Positioning cube ${i} at xOffset: ${xOffset}, yOffset: ${height / 2}, zOffset: ${zOffset}`);
+    cubes.setMatrixAt(i, new THREE.Matrix4().makeScale(width, height, depth));
+    cubes.setColorAt(i, new THREE.Color(`hsl(${color}, 50%, 50%)`));
+      cubes.setMatrixAt(i, new THREE.Matrix4().makeTranslation(xOffset, height / 2, zOffset));
+      cubes.instanceMatrix.needsUpdate = true;
+  
+      xOffset += width;
+      if (xOffset >= this.txnData.size) {
+        xOffset = 0;
+        zOffset += depth;
+      }
+  
+    //   console.log(`After incrementing: xOffset: ${xOffset}, zOffset: ${zOffset}`);
+    }
+  
+    // update the cubes instance matrix
+    cubes.instanceMatrix.needsUpdate = true;
+    this.terrain.add(cubes);
+    this.scene.add(this.terrain);
+    this.camera.lookAt(this.terrain.position);
+  }
+  
+
+  getInstancedCubes(instances) {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    //geometry.translate(-1 / 2, 0, -1 / 2);
+    const material = new THREE.MeshStandardMaterial({
+        // color: 0x00ffff,
+        roughness: 0.125,
+        metalness: 0.875,
+        opacity: 0.675,
+    });
     return new THREE.InstancedMesh(geometry, material, instances);
   }
 
